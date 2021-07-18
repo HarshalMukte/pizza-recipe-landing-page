3212b4e0ad48e2704e067c9bb9c4ddd5619e1650
Harshal <harshalmukte@gmail.com>
2021-07-18 18:44:41 +0530

    improvements

--- index.js
+++ index.js
@@ -23,34 +23,26 @@ function active(){
     menu.classList.toggle("active");
     social_media.classList.toggle("active");
 }
-// document.addEventListener("mouseup", () => {
-//     if(menu.classList.contains("active")){
-//         active();
-//     }
-// })
 
 
-// window.onclick = function(event) {
 
-//     if (event.target == images_modal) {
- 
-//        images_modal.style.display = "none";
- 
-//      }
- 
-//      if (event.target == videos_modal) {
- 
-//        videos_modal.style.display = "none";
- 
-//      }
- 
-//  }
-// window.onclick = function(event) {
+// go to top button 
 
-//     if (event.target != menuIcon) {
- 
-//        active();
- 
-//      }
+const mybutton = document.getElementById("myBtn");
 
-//  }
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
